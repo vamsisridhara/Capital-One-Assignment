2117c18683eb881500436a8f092bf455c05fb617
Vamsi Krishna Sridhara <vamsisridhara@gmail.com>
2015-11-19 22:26:31 -0500

    adding progressbar demo
    adding progressbar demo

---
+++ CommuterSystem/app/products/productListCtrl.js
@@ -0,0 +1,38 @@
+﻿(function () {
+    "use strict";
+    var app = angular.module("productManagement")
+                .controller("ProductListCtrl", ProductListCtrl);
+
+    function ProductListCtrl(){
+        var vm = this;
+        vm.products = [
+                    {
+                        "productID": "1",
+                        "productName": "Leaf Bake",
+                        "productCode": "GDN-0011",
+                        "releaseDate": "March 19 2009",
+                        "description": "Leaf",
+                        "cost": 9.00,
+                        "price": 19.95,
+                        "category": "garden",
+                        "tags": ["leaf", "tool"],
+                        "imageURL": "http://"
+                    },
+                    {
+                        "productID": "5",
+                        "productName": "Leaf Bake",
+                        "productCode": "GDN-0011",
+                        "releaseDate": "March 19 2009",
+                        "description": "Leaf",
+                        "cost": 9.00,
+                        "price": 19.95,
+                        "category": "garden",
+                        "tags": ["leaf", "tool"],
+                        "imageURL": "http://"
+
+                    }
+
+        ];
+
+    }
+})();
