20da1840838495a6795d0d339a2b95e58fd1f4fa
Vamsi Krishna Sridhara <vamsisridhara@gmail.com>
2015-11-30 22:58:10 -0500

    added unity, autofac, ninject

---
+++ GFFIAdmin/Autofac/AutofacScopeContainer.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http.Dependencies;
+using Autofac;
+using Autofac.Integration.Mvc;
+
+namespace GFFIAdmin.Autofac
+{
+    /// <summary>
+    /// Autofac Scope Container
+    /// </summary>
+    public class AutofacScopeContainer : IDependencyScope
+    {
+        /// <summary>
+        /// Private _container field
+        /// </summary>
+        private readonly AutofacDependencyResolver _container;
+
+        
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AutofacScopeContainer"/> class.
+        /// </summary>
+        /// <param name="container">The container.</param>
+        /// <exception cref="System.ArgumentNullException">container</exception>
+        protected AutofacScopeContainer(AutofacDependencyResolver container)
+        {
+            if (container == null)
+            {
+                throw new ArgumentNullException("container");
+            }
+            _container = container;
+        }
+
+        /// <summary>
+        /// Retrieves a service from the scope.
+        /// </summary>
+        /// <param name="serviceType">The service to be retrieved.</param>
+        /// <returns>
+        /// The retrieved service.
+        /// </returns>
+        public object GetService(Type serviceType)
+        {
+            return _container.ApplicationContainer.IsRegistered(serviceType) ? _container.GetService(serviceType) : null;
+        }
+
+        /// <summary>
+        /// Retrieves a collection of services from the scope.
+        /// </summary>
+        /// <param name="serviceType">The collection of services to be retrieved.</param>
+        /// <returns>
+        /// The retrieved collection of services.
+        /// </returns>
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return _container.ApplicationContainer.IsRegistered(serviceType) ? _container.GetServices(serviceType) : new List<object>();
+        }
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+            _container.ApplicationContainer.Dispose();
+        }
+
+    }
+
+    /// <summary>
+    /// Auto FacContainer
+    /// </summary>
+    public class AutoFacContainer : AutofacScopeContainer, IDependencyResolver
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AutoFacContainer"/> class.
+        /// </summary>
+        /// <param name="container">The container.</param>
+        public AutoFacContainer(AutofacDependencyResolver container)
+            : base(container)
+        {
+        }
+
+        /// <summary>
+        /// Starts a resolution scope.
+        /// </summary>
+        /// <returns>
+        /// The dependency scope.
+        /// </returns>
+        public IDependencyScope BeginScope()
+        {
+            return this;
+        }
+    }
+}
---
+++ GFFIAdmin/Controllers/AutofacTestController.cs
@@ -0,0 +1,41 @@
+﻿using GFFIAdmin.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GFFIAdmin.Controllers
+{
+    public class AutofacTestController : Controller
+    {
+        /// <summary>
+        /// Private _container field
+        /// </summary>
+        private readonly ICustomerAutofacRepository _customerRepository;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HomeController"/> class.
+        /// </summary>
+        /// <param name="customerRepository">The customer repository.</param>
+        public AutofacTestController(ICustomerAutofacRepository customerRepository)
+        {
+            _customerRepository = customerRepository;
+        }
+
+        // GET: /Customer/
+
+        /// <summary>
+        /// Method for index action.
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            //@model AutofacDemo.Customer
+            var customer = _customerRepository.GetCustomerDetails();
+
+            // View Index.cshtml
+            return View(customer);
+        }
+    }
+}
